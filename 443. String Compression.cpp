31906152e44bf21b2393226ca1c75d509ada66a5
TanyaSuryavanshi <109675038+TanyaSuryavanshi@users.noreply.github.com>
2023-01-29 15:01:05 +0530

    Create 443. String Compression.cpp

---
+++ 443. String Compression.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        int ansindex=0;
+        int n=chars.size();
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+
+            chars[ansindex++]=chars[i];
+            int count=j-i;
+            if(count>1){
+                //converting count in single digit and saving in char
+                string cnt=to_string(count);
+                for(char ch:cnt){
+                    chars[ansindex++]=ch;
+
+                }
+            }
+            i=j;
+        }
+        return ansindex;
+    }
+};
