a77afe1d9e3333cec7a3089e067734524340a590
TanyaSuryavanshi <109675038+TanyaSuryavanshi@users.noreply.github.com>
2023-03-22 01:31:05 +0530

    Create 78. Subsets.cpp

---
+++ 78. Subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(vector<int>nums,vector<vector<int>> &ans,vector<int> output,int index){
+        //basecase
+        if(index>=nums.size()){
+            ans.push_back(output);
+            return;
+        }
+
+        //exclude
+        solve(nums,ans,output,index+1);
+
+        //include
+        int element=nums[index];
+        output.push_back(element);
+        solve(nums,ans,output,index+1);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        solve(nums,ans,output,index);
+        return ans;
+    }
+};
